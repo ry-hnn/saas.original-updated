9f04b4ca9ba176fa1fccbec4ecbf0c12d4f5c78a
Ryan <rhenne300@gmail.com>
2024-08-21 13:35:03 -0400

    iconsupdated.menu

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const menuClose = document.querySelector(".menu-close");
 const menu = document.querySelector(".nav__mobile__links--container");
 
 function menuToggle() {
-    if (menu.style.display === "none") {
-        menu.style.display = "block";
-        menuOpen.style.display = "none";
-        menuClose.style.display = "block";
-    } else {
-        menu.style.display = "none";
-        menuOpen.style.display = "block";
-        menuClose.style.display = "none";
-    }
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+    menuOpen.style.display = "none";
+    menuClose.style.display = "block";
+  } else {
+    menu.style.display = "none";
+    menuOpen.style.display = "block";
+    menuClose.style.display = "none";
+  }
 }
